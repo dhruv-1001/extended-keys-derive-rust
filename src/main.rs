4ec36b98c93743c947c0f6ba24d1d2593b82a14b
dhruvbaliyan <dhruvbaliyan1001@gmail.com>
2022-06-22 11:50:37 +0530

    Added Driver code

--- src/main.rs
+++ src/main.rs
@@ -19,8 +19,13 @@ fn main() {
     let extended_priv_key = ExtendedPrivKey::new(
         Network::Testnet, 
         mnemonic, None
-    );
-    println!("{:?}", extended_priv_key.unwrap().xprv);
+    ).unwrap();
+    println!("{:?}", extended_priv_key.to_string());
+    let derivation_path = DerivationPath::new("m/84'/1'/0'/0".to_string()).unwrap();
+    let derived_priv_key = extended_priv_key.derive_xprv(&derivation_path);
+    println!("{:?}", derived_priv_key.to_string());
+    let derived_pub_key = extended_priv_key.derive_xpub(derivation_path);
+    println!("{:?}", derived_pub_key.to_string())
 }
 
 fn generate_mnemonic(
@@ -30,10 +35,12 @@ fn generate_mnemonic(
     Ok(mnemonic.into_key())
 }
 
+#[allow(dead_code)]
 struct DerivationPath{
     derivation_path: Mutex<BdkDerivationPath>,
 }
 
+#[allow(dead_code)]
 impl DerivationPath {
     fn new(
         path: String,
@@ -51,6 +58,7 @@ struct ExtendedPrivKey {
     xprv: Mutex<BdkExtendedPrivKey>,
 }
 
+#[allow(dead_code)]
 impl ExtendedPrivKey {
     fn new(
         network: Network,
@@ -66,7 +74,7 @@ impl ExtendedPrivKey {
 
     fn derive_xprv(
         &self,
-        path: DerivationPath,
+        path: &DerivationPath,
     ) -> ExtendedPrivKey {
         let secp = Secp256k1::new();
         let path = path.derivation_path.lock().unwrap().clone();
@@ -76,20 +84,29 @@ impl ExtendedPrivKey {
         ExtendedPrivKey { xprv: derived_xprv }
     }
 
-    // fn derive_xpub(
-    //     &self,
-    //     derivation_path: DerivationPath,
-    // ) -> ExtendedPubKey {
+    fn derive_xpub(
+        &self,
+        path: DerivationPath,
+    ) -> ExtendedPubKey {
+        let secp = Secp256k1::new();
+        let path = path.derivation_path.lock().unwrap().clone();
+        let derived_xprv = Mutex::new(
+            self.xprv.lock().unwrap().derive_priv(&secp, &path).unwrap()
+        );
+        let derived_xpub = Mutex::new(
+            BdkExtendedPubKey::from_private(&secp, &derived_xprv.lock().unwrap())
+        );
+        ExtendedPubKey { xpub: derived_xpub }
+    }
 
-    // }
-
-    // fn to_string(
-    //     &self,
-    // ) -> String {
-
-    // }
+    fn to_string(
+        &self,
+    ) -> String {
+        self.xprv.lock().unwrap().to_string()
+    }
 }
 
+#[allow(dead_code)]
 struct ExtendedPubKey {
     xpub: Mutex<BdkExtendedPubKey>,
 }
@@ -98,6 +115,6 @@ impl ExtendedPubKey{
     fn to_string(
         &self
     ) -> String {
-        "".to_string()
+        self.xpub.lock().unwrap().to_string()
     }
 }
